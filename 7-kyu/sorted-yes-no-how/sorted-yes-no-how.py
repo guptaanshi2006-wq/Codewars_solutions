1af6466cfef86aa65741f175549b16003a80eebf
guptaanshi2006-wq <gupta.anshi2006@gmail.com>
2026-09-18 20:18:46 +0530

    Add solution - CodeHub

---
+++ 7-kyu/sorted-yes-no-how/sorted-yes-no-how.py
@@ -0,0 +1,9 @@
+def is_sorted_and_how(arr):
+    # your code here
+    for i in arr:
+        if arr==sorted(arr):
+            return "yes, ascending"
+        elif arr==sorted(arr, reverse=True):
+            return "yes, descending"
+        else:
+            return "no"
