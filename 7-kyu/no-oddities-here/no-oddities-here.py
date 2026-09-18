867ca335bbbb11eb79a5c63d70388cf3c2eb0816
guptaanshi2006-wq <gupta.anshi2006@gmail.com>
2026-09-18 20:09:16 +0530

    Add solution - CodeHub

---
+++ 7-kyu/no-oddities-here/no-oddities-here.py
@@ -0,0 +1,9 @@
+def no_odds(values):
+    # Return list of only even values
+    value=[]
+    for i in values:
+        if i%2 ==0:
+            value.append(i)
+    return value
+            
+    
